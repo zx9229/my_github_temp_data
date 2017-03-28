00590f5ddcdd29be8a074476de3a01c7611f5590
zx9229 <zx9229@163.com>
2017-03-28 23:57:00 +0800

    commit

--- my_new_net/my_new_net/xx.hpp
+++ my_new_net/my_new_net/xx.hpp
@@ -14,62 +14,74 @@ using BoostSocketPtr = std::unique_ptr<boost::asio::ip::tcp::socket>;
 class TcpSocket
 {
 private:
-    class SendBuffer
-    {
-    public:
-        SendBuffer() :m_capacity(1024 * 512), m_isSending(false) { reset(); }
-    public:
-        void reset();
-    public:
-        const std::size_t m_capacity;
-        std::mutex  m_mutex;
-        std::string m_bufWait;
-        std::string m_bufWork;
-        std::size_t m_posWork;//当前的位置,再往前的数据都是被使用过的了,都是过时的数据了.
-        bool        m_isSending;
-    };
-    class RecvBuffer
-    {
-    public:
-        RecvBuffer() :m_capacity(1024 * 512), m_sortSize(1024 * 4), m_isRecving(false) { reset(); }
-    public:
-        void reset();
-    public:
-        const std::size_t m_capacity;
-        const std::size_t m_sortSize;//(右边的)剩余空间<=m_sortSize时,进行内存整理
-        std::mutex  m_mutex;
-        std::string m_buf;
-        std::size_t m_posBeg;
-        std::size_t m_posEnd;
-        bool        m_isRecving;
-    };
+	class ErrorId
+	{
+	public:
+		ErrorId() :m_id(0) {}
+	public:
+		int getNextId(int no);
+	private:
+		std::mutex m_mutex;
+		int m_id;
+	};
+	class SendBuffer
+	{
+	public:
+		SendBuffer() :m_capacity(1024 * 512), m_isSending(false) { reset(); }
+	public:
+		void reset();
+	public:
+		const std::size_t m_capacity;
+		std::mutex  m_mutex;
+		std::string m_bufWait;
+		std::string m_bufWork;
+		std::size_t m_posWork;//当前的位置,再往前的数据都被发送过了,都是过时的数据了.
+		bool        m_isSending;
+	};
+	class RecvBuffer
+	{
+	public:
+		RecvBuffer() :m_capacity(1024 * 512), m_sortSize(1024 * 4), m_isRecving(false) { reset(); }
+	public:
+		void reset();
+	public:
+		const std::size_t m_capacity;
+		const std::size_t m_sortSize;//(右边的)剩余空间<=m_sortSize时,进行内存整理
+		std::mutex  m_mutex;
+		std::string m_buf;
+		std::size_t m_posBeg;
+		std::size_t m_posEnd;
+		bool        m_isRecving;
+	};
 public:
-    TcpSocket(boost::asio::io_service& io);
-    TcpSocket(BoostSocketPtr& sock);
+	TcpSocket(boost::asio::io_service& io);
+	TcpSocket(BoostSocketPtr& sock);
 public:
-    bool isOpen()const;
-    void close();
-    boost::asio::ip::tcp::endpoint localPoint();
-    boost::asio::ip::tcp::endpoint remotePoint();
-    int connect(const std::string& ip, std::uint16_t port);
-    void send(const char* p, std::uint32_t len);
-    void startRecvAsync();
-    void onRtnStream() {}
-    void onRtnMessage() {}
-    void onError(int errId, const std::string& errMsg) {}
+	bool isOpen() const;
+	void close();
+	boost::asio::ip::tcp::endpoint localPoint();
+	boost::asio::ip::tcp::endpoint remotePoint();
+	int connect(const std::string& ip, std::uint16_t port);
+	void send(const char* p, std::uint32_t len);
+	void startRecvAsync();
+	void onRtnStream() {}
+	void onRtnMessage() {}
+	void onError(int errId, std::string errMsg) {}
+	void onConnected(){}
 private:
-    void doReconnect(const boost::system::error_code& ec);
-    void sendLoop(std::size_t lastSendLength);
-    void sendLoopHandler(const boost::system::error_code& ec, std::size_t bytes_transferred);
-    void recvAsync();
-    void recvHandler(const boost::system::error_code& error, std::size_t bytes_transferred);
+	void doReconnect(const boost::system::error_code& ec);
+	void sendAsync(std::size_t lastLengthSent);
+	void sendAsyncHandler(const boost::system::error_code& ec, std::size_t bytes_transferred);
+	void recvAsync();
+	void recvAsyncHandler(const boost::system::error_code& error, std::size_t bytes_transferred);
 private:
-    boost::asio::strand m_strand;
-    BoostSocketPtr m_socket;//要求:类创建完成后,指针肯定不为空,socket一定是存在的.
-    boost::asio::steady_timer m_timer;
-    boost::asio::ip::tcp::endpoint m_ep;
-    SendBuffer m_bufSend;
-    RecvBuffer m_bufRecv;
+	boost::asio::strand m_strand;
+	BoostSocketPtr m_socket;//要求:类创建完成后,指针肯定不为空,socket一定是存在的.
+	boost::asio::steady_timer m_timer;
+	boost::asio::ip::tcp::endpoint m_ep;
+	SendBuffer m_bufSend;
+	RecvBuffer m_bufRecv;
+	ErrorId m_errId;
 };
 #include "xx_impl.hpp"
 #endif//XX_HPP
--- my_new_net/my_new_net/xx_impl.hpp
+++ my_new_net/my_new_net/xx_impl.hpp
@@ -4,42 +4,53 @@
 
 #if 1 //TcpSocket
 
+#if 1 //TcpSocket::ErrorId
+int TcpSocket::ErrorId::getNextId(int no)
+{
+	const int& area = 100000;
+	std::lock_guard<std::mutex> lg(m_mutex);
+	no = std::abs(no) % area;
+	m_id = ++m_id % ((21 * 10000 * 10000) / area);
+	return -1 * (m_id*area + no);
+}
+#endif//TcpSocket::ErrorId
+
 #if 1 //TcpSocket::SendBuffer
 void TcpSocket::SendBuffer::reset()
 {
-    std::lock_guard<std::mutex> lg(m_mutex);
-    m_bufWait.clear();
-    m_bufWork.clear();
-    m_posWork = 0;
-    if (m_bufWait.capacity() < m_capacity)
-        m_bufWait.reserve(m_capacity);
-    if (m_bufWork.capacity() < m_capacity)
-        m_bufWork.reserve(m_capacity);
+	std::lock_guard<std::mutex> lg(m_mutex);
+	m_bufWait.clear();
+	m_bufWork.clear();
+	m_posWork = 0;
+	if (m_bufWait.capacity() < m_capacity)
+		m_bufWait.reserve(m_capacity);
+	if (m_bufWork.capacity() < m_capacity)
+		m_bufWork.reserve(m_capacity);
 }
 #endif//TcpSocket::SendBuffer
 
 #if 1 //TcpSocket::RecvBuffer
 void TcpSocket::RecvBuffer::reset()
 {
-    std::lock_guard<std::mutex> lg(m_mutex);
-    if (m_buf.size() < m_capacity)
-    { //resize时,可能会对老的数据执行copy操作,clear后就不会copy了,
-        m_buf.clear();
-        m_buf.resize(m_capacity);
-    }
-    m_posBeg = 0;
-    m_posEnd = 0;
+	std::lock_guard<std::mutex> lg(m_mutex);
+	if (m_buf.size() < m_capacity)
+	{ //resize时,可能会对老的数据执行copy操作,clear之后就不会copy了.
+		m_buf.clear();
+		m_buf.resize(m_capacity);
+	}
+	m_posBeg = 0;
+	m_posEnd = 0;
 }
 #endif//TcpSocket::RecvBuffer
 
-TcpSocket::TcpSocket(boost::asio::io_service& io) : m_timer(io), m_strand(io)
+TcpSocket::TcpSocket(boost::asio::io_service& io) : m_strand(io), m_timer(io)
 {
-    m_socket = BoostSocketPtr(new boost::asio::ip::tcp::socket(io));
+	m_socket = BoostSocketPtr(new boost::asio::ip::tcp::socket(io));
 }
 
-TcpSocket::TcpSocket(BoostSocketPtr& sock) : m_timer(sock->get_io_service()), m_strand(sock->get_io_service())
+TcpSocket::TcpSocket(BoostSocketPtr& sock) : m_strand(sock->get_io_service()), m_timer(sock->get_io_service())
 {
-    m_socket = std::move(sock);
+	m_socket = std::move(sock);
 }
 
 bool TcpSocket::isOpen() const
@@ -51,7 +62,8 @@ void TcpSocket::close()
 {
     m_ep = boost::asio::ip::tcp::endpoint();
     m_socket->close();
-    //接收线程可能正在接收数据,或处理数据.
+    //TODO:接收线程可能正在接收数据,或处理数据.
+	//TODO:清理发送缓冲区和接收缓冲区.
 }
 
 boost::asio::ip::tcp::endpoint TcpSocket::localPoint()
@@ -66,45 +78,47 @@ boost::asio::ip::tcp::endpoint TcpSocket::remotePoint()
 
 int TcpSocket::connect(const std::string& ip, std::uint16_t port)
 {
-    boost::system::error_code ec;
-    boost::asio::ip::address addr = boost::asio::ip::address::from_string(ip, ec);
-    if (ec)
-    {
-        onError(ec.value(), ec.message());
-        return -1;
-    }
-    m_ep.address(addr);
-    m_ep.port(port);
-    m_timer.expires_from_now(std::chrono::milliseconds(0));
-    m_timer.async_wait(boost::bind(&TcpSocket::doReconnect, this, boost::asio::placeholders::error));
-    return 0;
+	int errId = 0;
+	boost::system::error_code ec;
+	boost::asio::ip::address addr = boost::asio::ip::address::from_string(ip, ec);
+	if (ec)
+	{
+		errId = m_errId.getNextId(ec.value());
+		m_strand.post(boost::bind(&TcpSocket::onError, this, errId, ec.message()));
+		return errId;
+	}
+	m_ep.address(addr);
+	m_ep.port(port);
+	m_timer.expires_from_now(std::chrono::milliseconds(0));
+	m_timer.async_wait(boost::bind(&TcpSocket::doReconnect, this, boost::asio::placeholders::error));
+	return errId;
 }
 
 void TcpSocket::doReconnect(const boost::system::error_code& ec)
 {
-    if (ec)
-    {
-        onError(ec.value(), ec.message());
-        return;
-    }
-    if (m_ep.port() == 0)
-    {
-        std::string msg = "boost::asio::ip::tcp::endpoint abnormal, port==0.";
-        onError(-1, msg);
-        return;
-    }
-    boost::system::error_code err;
-    m_socket->connect(m_ep, err);
-    if (err)
-    {
-        onError(err.value(), err.message());
-        m_timer.expires_from_now(std::chrono::milliseconds(5000));
-        m_timer.async_wait(boost::bind(&TcpSocket::doReconnect, this, boost::asio::placeholders::error));
-    }
-    else
-    {
-        //do sth.
-    }
+	if (ec)
+	{
+		m_strand.post(boost::bind(&TcpSocket::onError, this, m_errId.getNextId(ec.value()), ec.message()));
+		return;
+	}
+	if (m_ep.port() == 0)
+	{
+		std::string msg = "boost::asio::ip::tcp::endpoint abnormal, port==0.";
+		m_strand.post(boost::bind(&TcpSocket::onError, this, m_errId.getNextId(-1), msg));
+		return;
+	}
+	boost::system::error_code err;
+	m_socket->connect(m_ep, err);
+	if (err)
+	{
+		m_strand.post(boost::bind(&TcpSocket::onError, this, m_errId.getNextId(err.value()), err.message()));
+		m_timer.expires_from_now(std::chrono::milliseconds(5000));
+		m_timer.async_wait(boost::bind(&TcpSocket::doReconnect, this, boost::asio::placeholders::error));
+	}
+	else
+	{
+		m_strand.post(boost::bind(&TcpSocket::onConnected, this));
+	}
 }
 
 void TcpSocket::send(const char* p, std::uint32_t len)
@@ -116,18 +130,18 @@ void TcpSocket::send(const char* p, std::uint32_t len)
         if (m_bufSend.m_isSending)
             return;
     }
-    sendLoop(0);
+    sendAsync(0);
 }
 
-void TcpSocket::sendLoop(std::size_t lastSendLength)
+void TcpSocket::sendAsync(std::size_t lastLengthSent)
 {
-    const char* pData;
-    std::uint32_t dataLen;
+	const char* p = nullptr;
+	std::uint32_t len = 0;
     if (true)
     {
         std::lock_guard<std::mutex> lg(m_bufSend.m_mutex);
-        m_bufSend.m_posWork += lastSendLength;
-        assert(m_bufSend.m_posWork < m_bufSend.m_bufWork.size());
+        m_bufSend.m_posWork += lastLengthSent;
+        assert(m_bufSend.m_posWork <= m_bufSend.m_bufWork.size());
 
         if (m_bufSend.m_bufWork.size() == m_bufSend.m_posWork)
         {
@@ -135,9 +149,9 @@ void TcpSocket::sendLoop(std::size_t lastSendLength)
             m_bufSend.m_posWork = 0;
             std::swap(m_bufSend.m_bufWork, m_bufSend.m_bufWait);
         }
-        pData = &m_bufSend.m_bufWork[m_bufSend.m_posWork];
-        dataLen = m_bufSend.m_bufWork.size() - m_bufSend.m_posWork;
-        if (dataLen)
+        p = &m_bufSend.m_bufWork[m_bufSend.m_posWork];
+        len = m_bufSend.m_bufWork.size() - m_bufSend.m_posWork;
+        if (len)
         {
             m_bufSend.m_isSending = true;
         }
@@ -148,17 +162,17 @@ void TcpSocket::sendLoop(std::size_t lastSendLength)
         }
     }
     //基于什么思考,要使用m_strand.wrap包装一下handler呢?
-    m_socket->async_send(boost::asio::buffer(pData, dataLen), m_strand.wrap(/*用std::bind会出错*/boost::bind(&TcpSocket::sendLoopHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)));
+    m_socket->async_send(boost::asio::buffer(p, len), m_strand.wrap(/*用std::bind会出错*/boost::bind(&TcpSocket::sendAsyncHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)));
 }
 
-void TcpSocket::sendLoopHandler(const boost::system::error_code& ec, std::size_t bytes_transferred)
+void TcpSocket::sendAsyncHandler(const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
     if (ec)
     {
-        onError(ec.value(), ec.message());
+		m_strand.post(boost::bind(&TcpSocket::onError, this, m_errId.getNextId(ec.value()), ec.message()));
         return;
     }
-    sendLoop(bytes_transferred);
+    sendAsync(bytes_transferred);
 }
 
 void TcpSocket::startRecvAsync()
@@ -176,47 +190,40 @@ void TcpSocket::startRecvAsync()
 
 void TcpSocket::recvAsync()
 {
-    char* dataHead = nullptr;
-    std::uint32_t dataSize = 0;
+	char* p = nullptr;
+	std::uint32_t len = 0;
 
-    if (m_bufRecv.m_posEnd + m_bufRecv.m_sortSize < m_bufRecv.m_buf.size())
-    {//(右边的)剩余空间>m_sortSize时,就直接使用剩余空间
-        dataHead = &m_bufRecv.m_buf[m_bufRecv.m_posEnd];
-        dataSize = m_bufRecv.m_buf.size() - m_bufRecv.m_posEnd;
-    }
-    else if (0 < m_bufRecv.m_posBeg)
-    {//(右边的)剩余空间<=m_sortSize时,就整理内存,然后使用整理后的剩余空间
-        memcpy(&m_bufRecv.m_buf[0], &m_bufRecv.m_buf[m_bufRecv.m_posBeg], m_bufRecv.m_posEnd - m_bufRecv.m_posBeg);
-        m_bufRecv.m_posEnd = m_bufRecv.m_posEnd - m_bufRecv.m_posBeg;
-        m_bufRecv.m_posBeg = 0;
-        //
-        dataHead = &m_bufRecv.m_buf[m_bufRecv.m_posEnd];
-        dataSize = m_bufRecv.m_buf.size() - m_bufRecv.m_posEnd;
-    }
-    else
-    {
-        assert(false);
-    }
-    m_socket->async_receive(boost::asio::buffer(dataHead, dataSize), m_strand.wrap(boost::bind(&TcpSocket::recvHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)));
+	if ((m_bufRecv.m_buf.size() - m_bufRecv.m_posEnd < m_bufRecv.m_sortSize) &&
+		(0 < m_bufRecv.m_posBeg))
+	{ //(右边的)剩余空间<m_sortSize,而且可以整理内存,那么就整理内存.
+		memcpy(&m_bufRecv.m_buf[0], &m_bufRecv.m_buf[m_bufRecv.m_posBeg], m_bufRecv.m_posEnd - m_bufRecv.m_posBeg);
+		m_bufRecv.m_posEnd = m_bufRecv.m_posEnd - m_bufRecv.m_posBeg;
+		m_bufRecv.m_posBeg = 0;
+	}
+	p = &m_bufRecv.m_buf[m_bufRecv.m_posEnd];
+	len = m_bufRecv.m_buf.size() - m_bufRecv.m_posEnd;
+	if (!len)
+	{
+		assert(false);
+	}
+	m_socket->async_receive(boost::asio::buffer(p, len), m_strand.wrap(boost::bind(&TcpSocket::recvAsyncHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred)));
 }
 
-void TcpSocket::recvHandler(const boost::system::error_code& ec, std::size_t bytes_transferred)
+void TcpSocket::recvAsyncHandler(const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
-    if (true)
-    {
-        std::lock_guard<std::mutex> lg(m_bufRecv.m_mutex);
-        if (ec)
-        {
-            m_bufRecv.m_isRecving = false;
-            return;
-        }
-        m_bufRecv.m_posEnd += bytes_transferred;
-    }
-    recvAsync();
+	if (true)
+	{
+		std::lock_guard<std::mutex> lg(m_bufRecv.m_mutex);
+		if (ec)
+		{
+			//TODO:断线.
+			m_bufRecv.m_isRecving = false;
+			return;
+		}
+		m_bufRecv.m_posEnd += bytes_transferred;
+	}
+	recvAsync();
 }
-
-
 #endif//TcpSocket
-
 
 #endif//xx_impl_H
